9ce45a8e22e5e3a3b14a7d8e27ed4d4b2e03eb6e
SRubiRosales <sharon_rosales98@hotmail.com>
2023-09-15 13:50:16 -0600

    Add method

--- src/Automovil.java
+++ src/Automovil.java
@@ -3,4 +3,13 @@ public class Automovil {
     String modelo;
     String color = "Gris";
     double cinlindrada;
+
+    public String detalle() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\nauto.fabricante = " + this.fabricante);
+        sb.append("\nauto.modelo = " + this.modelo);
+        sb.append("\nauto.color = " + this.color);
+        sb.append("\nauto.cinlindrada = " + this.cinlindrada);
+        return sb.toString();
+    }
 }
--- src/EjemploAutomovil.java
+++ src/EjemploAutomovil.java
@@ -1,9 +1,9 @@
 public class EjemploAutomovil {
     public static void main(String[] args) {
-        Automovil auto = new Automovil();
-        auto.fabricante = "Nissan";
-        auto.modelo = "Tsuru";
-        auto.cinlindrada = 1.8;
+        Automovil tsuru = new Automovil();
+        tsuru.fabricante = "Nissan";
+        tsuru.modelo = "Tsuru";
+        tsuru.cinlindrada = 1.8;
 
         Automovil mazda = new Automovil();
         mazda.fabricante = "Mazda";
@@ -11,14 +11,7 @@ public class EjemploAutomovil {
         mazda.cinlindrada = 2.0;
         mazda.color = "Rojo";
 
-        System.out.println("auto.fabricante " + auto.fabricante);
-        System.out.println("auto.modelo = " + auto.modelo);
-        System.out.println("auto.color = " + auto.color);
-        System.out.println("auto.cinlindrada = " + auto.cinlindrada);
-
-        System.out.println("mazda.fabricante " + mazda.fabricante);
-        System.out.println("mazda.modelo = " + mazda.modelo);
-        System.out.println("mazda.color = " + mazda.color);
-        System.out.println("mazda.cinlindrada = " + mazda.cinlindrada);
+        System.out.println(tsuru.detalle());
+        System.out.println(mazda.detalle());
     }
 }
